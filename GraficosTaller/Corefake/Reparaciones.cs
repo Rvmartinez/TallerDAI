2c08b97675ac3ff9628ae404c8fac0f2d1b14a69
Alv-Maker <113826878+Alv-Maker@users.noreply.github.com>
2024-11-13 20:22:06 +0100

    Incorporando desglose por cliente #1
    Se incorporan cambios en el Corefake y en la ChartWindow que permitan añadir la nueva funcionalidad

--- GraficosTaller/Corefake/Reparaciones.cs
+++ GraficosTaller/Corefake/Reparaciones.cs
@@ -17,15 +17,43 @@ public class Reparaciones
     {
         ReparacionesLista.Add(reparacion);
     }
+    
+    
 
     public int GetReparacionesAnno(int anno, Boolean fin = false)
     {
         return ReparacionesLista.Count(reparacion => reparacion.GetAnno(fin) == anno);
     }
-
+    
     public int GetReparacionesMes(int mes, int anno, Boolean fin = false)
     {
         return ReparacionesLista.Count(reparacion => reparacion.GetMes(fin) == mes && reparacion.GetAnno(fin) == anno);
+    }
+    
+    public int GetReparacionesAnnoCliente(int anno, Cliente cliente, Boolean fin = false)
+    {
+        return ReparacionesLista.Count(reparacion => reparacion.GetAnno(fin) == anno && reparacion.Cliente == cliente);
+    }
+    
+    public Reparaciones GetReparacionesCliente(Cliente cliente)
+    {
+        Reparaciones reparaciones = new Reparaciones();
+        reparaciones.ReparacionesLista = ReparacionesLista.Where(reparacion => reparacion.Cliente == cliente).ToList();
+        return reparaciones;
+    }
+    
+    public String[] GetClientesReparaciones()
+    {
+        List<String> clientes = new List<String>();
+        foreach(var reparacion in ReparacionesLista)
+        {
+            if (!clientes.Contains(reparacion.Cliente.Nombre))
+            {
+                clientes.Add(reparacion.Cliente.Nombre);
+            }
+        }
+
+        return clientes.ToArray();
     }
 
     public int[] getAnnosReparaciones(Boolean fin = false)
